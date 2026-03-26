0c79f8d8aa770b7b88bab19dbc6d51b997e5c076
Akram Al Qasemi <akram.23.01.20@gmail.com>
2026-03-26 23:56:54 +0300

    update every thing depend on robot name

--- launch/robot_arm_three.launch.py
+++ launch/robot_arm_three.launch.py
@@ -1,31 +1,24 @@
-# Copyright 2021 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, AppendEnvironmentVariable
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
 
-    yaml_path  = "/home/ros2/Desktop/ros_ws/src/robot_arm_three/config/robot_arm_three.yaml"
+    pkg_share = get_package_share_directory('robot_arm_three')
 
-    # Declare arguments
+    yaml_path = os.path.join(pkg_share, 'config', 'robot_arm_three.yaml')
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH',
+        os.path.dirname(pkg_share)
+    )
+
     declared_arguments = []
     declared_arguments.append(
         DeclareLaunchArgument(
@@ -35,39 +28,29 @@ def generate_launch_description():
         )
     )
 
-    # Initialize Arguments
     gui = LaunchConfiguration("gui")
 
-    # gazebo
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
         ),
-        launch_arguments=[("gz_args", " -r -v 3  /home/ros2/Desktop/ros_ws/src/system_hss/launch/system_gazebo.sdf")],
+        launch_arguments=[("gz_args", f" -r -v 3 empty.sdf")],
         condition=IfCondition(gui),
     )
     gazebo_headless = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
         ),
-        launch_arguments=[("gz_args", ["--headless-rendering -s -r -v 3 /home/ros2/Desktop/ros_ws/src/system_hss/launch/system_gazebo.sdf"])],
+        launch_arguments=[("gz_args", f"--headless-rendering -s -r -v 3 empty.sdf")],
         condition=UnlessCondition(gui),
     )
-
-    # Gazebo bridge
-    # gazebo_bridge = Node(
-    #     package="ros_gz_bridge",
-    #     executable="parameter_bridge",
-    #     arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"],
-    #     output="screen",
-    # )
 
     gazebo_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=['--ros-args','-p', f'config_file:={yaml_path}'],
+        arguments=['--ros-args', '-p', f'config_file:={yaml_path}'],
         output='screen'
-        )
+    )
 
     gz_spawn_entity = Node(
         package="ros_gz_sim",
@@ -77,19 +60,18 @@ def generate_launch_description():
             "-topic",
             "/robot_description",
             "-name",
-            "rrbot_system_position",
+            "robot_arm_three_system_position",
             "-allow_renaming",
             "true",
         ],
     )
 
-    # Get URDF via xacro
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare("robot_arm_three"), "urdf", "rrbot.urdf.xacro"]
+                [FindPackageShare("robot_arm_three"), "urdf", "robot_arm_three_main.urdf.xacro"]
             ),
             " ",
             "use_gazebo:=true",
@@ -112,8 +94,6 @@ def generate_launch_description():
         ]
     )
 
-    rviz_config_file = "/home/ros2/Desktop/ros_ws/src/firstrobot/rviz/xacroconfigration.rviz"
-
     node_robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -126,55 +106,31 @@ def generate_launch_description():
         executable="spawner",
         arguments=["joint_state_broadcaster", "--param-file", robot_controllers2],
     )
-    
 
-    # trajectory_controller_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     arguments=["joint_trajectory_controller", "--param-file", robot_controllers],
-    # )
-
-
-    # robot_controller_spawner_position= Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     arguments=["forward_position_controller", "--param-file", robot_controllers2],
-    # )
     velocity_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["forward_velocity_controller", "--param-file", robot_controllers2],
     )
-    # controller_manager_node = Node(
-    #     package='controller_manager',
-    #     executable='ros2_control_node',
-    #     parameters=[robot_controllers],  # yaml dosyan
-    #     output='screen'
+
+    # rviz_node = Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     name="rviz2",
+    #     output="log",
+    #     arguments=["-d", rviz_config_file],
+    #     condition=IfCondition(gui),
     # )
 
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        condition=IfCondition(gui),
-    )
-
     nodes = [
+        set_env_vars_resources,        
         gazebo,
         gazebo_headless,
         gazebo_bridge,
         node_robot_state_publisher,
         gz_spawn_entity,
-        # controller_manager_node,
         joint_state_broadcaster_spawner,
-        # robot_controller_spawner,
-        # trajectory_controller_spawner,
-        # rviz_node,
         velocity_controller_spawner,
-        # robot_controller_spawner_position,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
-                                                                
